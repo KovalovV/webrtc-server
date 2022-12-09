8820d5480e59d51de20ba753c99d4dabebccf260
Vitaliy Kovalov <vitalya.kovalov@gmail.com>
2022-12-09 12:28:18 +0200

    disabled cors

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-	res.send('Running');
+  res.send('Running');
 });
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
 
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
 
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  });
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
